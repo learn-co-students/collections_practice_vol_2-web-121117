b5a5ef693700bd3072a3b7cf285fdded11f6e581
Kenny Hom <kenny.hom27@gmail.com>
2017-11-20 19:11:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
 # your code goes here
+def begins_with_r(array)
+  array.all? { |item| item[0] == "r"}
+end
+
+def contain_a(array)
+  array.select {|item| item.include?("a")}
+end
+
+def first_wa(array)
+  array.find { |item| item[0..1] == "wa"}
+end
+
+def remove_non_strings(array)
+  array.select { |item| item.instance_of?(String)}
+end
+
+def count_elements(array)
+  array.uniq.each { |item| item[:count] = array.count(item)}
+end
