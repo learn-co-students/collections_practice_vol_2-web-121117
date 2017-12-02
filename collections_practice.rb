76c4872d991963436cc6d5f65e9d1cca917deb60
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-12-02 22:07:04 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def begins_with_r(array)
   array.all? do |i|
     i.start_with?("r")
@@ -30,13 +32,58 @@ end
 
 def count_elements(array)
   output=[]
-  array.each do |i|
-    output << [i, array.count(i)]
-    array.delete_if(i)
+  array.each do |name_hash|
+    name_hash.each do |name_symbol,name|
+      if output.size == 0
+        output << {name: name, count: 0}
+      end
+      if output[0][:name] != name
+          output << {name: name, count: 1}
+      end
+      if output[0][:name] == name
+        output[0][:count] +=1
+      end
+    end
   end
   output
 end
 
-def merge_data(arr1,arr2)
-  arr1.concat(arr2)
+def merge_data(keys, data)
+  hash_array = [];
+
+  keys.each do |name|
+    first_name = name[:first_name]
+    data.each do |person|
+      first_name_data = person[first_name]
+      first_name_data[:first_name] = first_name
+      hash_array << person[first_name]
+    end
+  end
+  hash_array
+end
+
+def find_cool(array)
+  output=[]
+  array.each_with_index do |key,i|
+    key.each do |symbols, values|
+      if values == "cool"
+        output << array[i]
+      end
+    end
+  end
+  output
+end
+
+def organize_schools(school_hash)
+  new_hash = {}
+
+  school_hash.each do |school_name,location_hash|
+    location_hash.each do |location_symbol,city|
+      if !new_hash.include?(city)
+        new_hash[city] = []
+      end
+      new_hash[city] << school_name
+    end
+  end
+  new_hash
 end
